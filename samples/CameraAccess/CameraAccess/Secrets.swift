0bf3878f3a6483c5cc23b0d7dfe1c4b5272d0d6c
luisJAC1 <ljalfaro555@gmail.com>
2026-04-09 13:31:54 -0600

    Update geminiAPIKey in Secrets.swift

---
+++ samples/CameraAccess/CameraAccess/Secrets.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum Secrets {
+    static let geminiAPIKey = "AIzaSyAiLW-9BROiQGErZmzJNrIgkzgcRrAonWs"
+    static let openClawHost = "http://localhost"
+    static let openClawPort = 18789
+    static let openClawHookToken = ""
+    static let openClawGatewayToken = ""
+    static let webrtcSignalingURL = "ws://localhost:8080"
+}
